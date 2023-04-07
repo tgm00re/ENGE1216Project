67090e8161d0cb65fc2740139f22c2d8af8ceda7
tgm00re <tmoorecode@gmail.com>
2023-04-07 14:48:50 -0400

    added auto-generated spring boot project

---
+++ src/main/java/com/engeProject/engeProject/EngeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.engeProject.engeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engeProject/engeProject/EngeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engeProject.engeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
